f6b4577d002bda2bf7e32fdaf48cbadc24aca76d
joseninogarcia06 <jngarcia08081988@gmail.com>
2026-02-21 18:44:44 +0800

    Invalid docker file

--- SSMSWebAPI/obj/Debug/net10.0/SSMSWebAPI.AssemblyInfo.cs
+++ SSMSWebAPI/obj/Debug/net10.0/SSMSWebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SSMSWebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a798b97621b5fdbd02a9e65cececd1e19511bf6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+884aa2c8de2651bc1efd8cf39fb1e5cc03f84330")]
 [assembly: System.Reflection.AssemblyProductAttribute("SSMSWebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SSMSWebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
