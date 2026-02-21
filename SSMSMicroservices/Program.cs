9db8409c0ad770a359691c1ddc901b9a53f075c4
joseninogarcia06 <jngarcia08081988@gmail.com>
2026-02-21 19:42:57 +0800

    Update version

--- SSMSMicroservices/Program.cs
+++ SSMSMicroservices/Program.cs
@@ -3,6 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddOpenApi(); // now recognized
 
 var app = builder.Build();
 
@@ -33,6 +34,7 @@ app.MapGet("/weatherforecast", () =>
 })
 .WithName("GetWeatherForecast");
 
+app.MapOpenApi(); // now recognized
 app.Run();
 
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
